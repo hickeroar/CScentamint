a819259ace06a8325420f1eaaab9f4fa74499185
Hickeroar <ryan.vennell@gmail.com>
2015-06-11 18:21:42 -0500

    Adding initial files.

---
+++ CScentamint/Controllers/TrainController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CScentamint.Controllers
+{
+    public class TrainController : ApiController
+    {
+        // GET api/values
+        public ExpandoObject Get()
+        {
+            dynamic jsonObject = new ExpandoObject();
+            jsonObject.test = "foo";
+
+            return jsonObject;
+        }
+
+        // GET api/values/5
+        public int Get(int id)
+        {
+            return 3;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CScentamint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CScentamint.WebApiApplication" Language="C#" %>
