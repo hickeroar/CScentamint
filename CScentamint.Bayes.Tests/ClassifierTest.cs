5f49167099280c182fae933db5cb13e8a67f6728
Hickeroar <ryan.vennell@gmail.com>
2015-06-17 23:05:17 -0500

    Updating test suite to pass and adding bayes tests

---
+++ CScentamint.Bayes.Tests/ClassifierTest.cs
@@ -0,0 +1,33 @@
+﻿using CScentamint.Bayes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CScentamint.Bayes.Tests
+{
+    [TestClass]
+    public class ClassifierTest
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            Classifier.Categories = null;
+            Classifier.Probabilities = null;
+        }
+
+        [TestMethod]
+        public void TestClassifierStaticStorageIsInitializedOnInstantiation()
+        {
+            // Assert
+            Assert.IsNull(Classifier.Categories);
+            Assert.IsNull(Classifier.Probabilities);
+
+            // Arrange/Act
+            Classifier cls = new Classifier();
+
+            // Assert
+            Assert.IsInstanceOfType(Classifier.Categories, typeof(Dictionary<string, Dictionary<string, int>>));
+            Assert.IsInstanceOfType(Classifier.Probabilities, typeof(Dictionary<string, Dictionary<string, float>>));
+        }
+    }
+}
